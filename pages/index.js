ed1cd9ae654f2fe4b70dc7313c0d716b10a16b3e
Laptop Wala <muhammadomairakbar@gmail.com>
2024-06-23 22:29:14 +0500

    done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  let [fullScreen,setFullScreen] = useState(false)
   return (
     <body>
       <Head>
